ad09ec73a67933240ccce012eae6fefd8281982c
MrMohone <mohammedawol0837@gmail.com>
2025-07-02 01:10:14 +0300

    data fetched to frontend

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -5,4 +5,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id','name','slug','image','description','category','price']
+        fields = ['id','name','slug','image','descriptions','category','price']
--- shoppit/settings.py
+++ shoppit/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'core',
     'shop_app',
     'rest_framework',
+    'corsheaders',
     
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+]
+
 
 ROOT_URLCONF = 'shoppit.urls'
 
@@ -119,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'img/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
