9d818230855ffb8cf2fe901a65aa2fdf05ee5593
MrMohone <mohammedawol0837@gmail.com>
2025-07-05 00:37:21 +0300

    Product Detial are added

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -9,9 +9,11 @@ class ProductSerializer(serializers.ModelSerializer):
         
 class DetailedProductSerializer(serializers.ModelSerializer):
     similar_products = serializers.SerializerMethodField()
-    fields = ['id', 'name', 'price', 'slug', 'image', 'description', 'similar_products']
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price', 'slug', 'image', 'descriptions', 'similar_products']
     
     def get_similar_products(self, product):
-        products = Product.objects.filter(category=Product.category).exclude(id=product.id)
+        products = Product.objects.filter(category=product.category).exclude(id=product.id)
         serializer = ProductSerializer(products, many=True)
         return serializer.data
