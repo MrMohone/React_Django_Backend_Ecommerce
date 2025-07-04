2b33af5ef0486f3430bf62ad5b711e56845aab39
MrMohone <mohammedawol0837@gmail.com>
2025-07-04 23:10:46 +0300

    product detail tabel added

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -6,3 +6,12 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id','name','slug','image','descriptions','category','price']
+        
+class DetailedProductSerializer(serializers.ModelSerializer):
+    similar_products = serializers.SerializerMethodField()
+    fields = ['id', 'name', 'price', 'slug', 'image', 'description', 'similar_products']
+    
+    def get_similar_products(self, product):
+        products = Product.objects.filter(category=Product.category).exclude(id=product.id)
+        serializer = ProductSerializer(products, many=True)
+        return serializer.data
