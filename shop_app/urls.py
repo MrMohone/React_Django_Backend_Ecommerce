46b280719e9259aa16a438bb934615d0cc078ef6
MrMohone <mohammedawol0837@gmail.com>
2025-07-07 00:02:54 +0300

    add_cart url commited and backend for add_cart commit

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('products', views.products, name='products'),
     path('product_detail/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('add_item/', views.add_item, name='add_item'),
 ]
 
 # fetching all_products: http://127.0.0.1:8000/products
