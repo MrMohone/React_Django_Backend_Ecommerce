b2fb9fbfa3036bb9baebd1de83ec707fce0786c8
MrMohone <mohammedawol0837@gmail.com>
2025-07-14 03:43:15 +0300

    update cart is going well

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('product_in_cart/', views.product_in_cart, name='product_in_cart'),
     path('get_cart_stat/', views.get_cart_stat, name='get_cart_stat'),
     path('get_cart/', views.get_cart, name='get_cart_stat'),
+    path('update_quantity/', views.update_quantity, name='update_quantity'),
 ]
 
 # fetching all_products: http://127.0.0.1:8000/products
--- shop_app/views.py
+++ shop_app/views.py
@@ -64,3 +64,19 @@ def get_cart(request):
     
     serializer = CartSerializer(cart)
     return Response(serializer.data)
+
+
+@api_view(['PATCH'])# used to update specific field
+def update_quantity(request):
+    try:
+        cartitem_id = request.data.get('item_id')# get cartitem_id from front end
+        quantity = request.data.get('quantity')# get quantity from front end
+        quantity = int(quantity)# convert to integer
+        cartitem = CartItem.objects.get(id=cartitem_id)
+        cartitem.quantity = quantity
+        cartitem.save()
+        serializer = CartItemSerializer(cartitem)
+        return Response({'data': serializer.data, 'message': 'CartItem updated Successfully'}, status=200)
+    
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
